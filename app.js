32e4b658a209d07fb1906419f35b9f41f214738c
Franklin van de Meent <fr@nkl.in>
2015-02-03 13:22:16 +0100

    Code style, jshint

--- app.js
+++ app.js
@@ -7,28 +7,23 @@ Feedback:     https://github.com/fvdm/nodejs-toonapp/issues
 License:      Unlicense (Public Domain) -- see LICENSE file
 */
 
-var https = require('https')
-var querystring = require('querystring')
-var app = {}
-var user = {}
-var cache = null
 
 // Get version data
-app.version = function( cb ) {
-  talk({
+app.version = function (cb) {
+  talk ({
     method: 'GET',
     path: '/javascript/version.json',
     query: {
-      _: Date.now()
+      _: Date.now ()
     },
     complete: cb
-  })
-}
+  });
+};
 
 // Set temperature preset
-app.setPreset = function( preset, cb ) {
-  function run() {
-    talk({
+app.setPreset = function (preset, cb) {
+  function run () {
+    talk ({
       method: 'GET',
       path: '/toonMobileBackendWeb/client/auth/schemeState',
       query: {
@@ -36,90 +31,90 @@ app.setPreset = function( preset, cb ) {
         clientIdChecksum: cache.clientIdChecksum,
         state: 2,
         temperatureState: preset,
-        random: guidGenerator(),
-        _: Date.now()
+        random: guidGenerator (),
+        _: Date.now ()
       },
       complete: cb
-    })
-  }
+    });
+  };
 
-  if( cache ) {
-    run()
+  if (cache) {
+    run ();
   } else {
-    login( function( err, data ) {
-      if( err ) { cb( err ) }
-      run()
-    })
+    login (function (err, data) {
+      if (err) { cb (err); }
+      run ();
+    });
   }
-}
+};
 
 // Set manual temp, value = Celcius * 100, i.e. 1847 = 18.47C = 18.5 display
-app.setTemperature = function( value, cb ) {
-  function run() {
-    talk({
+app.setTemperature = function (value, cb) {
+  function run () {
+    talk ({
       method: 'GET',
       path: '/toonMobileBackendWeb/client/auth/setPoint',
       query: {
         clientId: cache.clientId,
         clientIdChecksum: cache.clientIdChecksum,
         value: value,
-        random: guidGenerator(),
-        _: Date.now()
+        random: guidGenerator (),
+        _: Date.now ()
       },
       complete: cb
-    })
+    });
   }
 
-  if( cache ) {
-    run()
+  if (cache) {
+    run ();
   } else {
-    login( function( err, data ) {
-      if( err ) { cb( err ) }
-      run()
-    })
+    login (function (err, data) {
+      if (err) { cb (err); }
+      run ();
+    });
   }
-}
+};
 
 // Get everything
-app.getState = function( cb ) {
-  function run() {
-    talk({
+app.getState = function (cb) {
+  function run () {
+    talk ({
       method: 'GET',
       path: '/toonMobileBackendWeb/client/auth/retrieveToonState',
       query: {
         clientId: cache.clientId,
         clientIdChecksum: cache.clientIdChecksum,
-        random: guidGenerator(),
-        _: Date.now()
+        random: guidGenerator (),
+        _: Date.now ()
       },
       complete: cb
-    })
+    });
   }
 
-  if( cache ) {
-    run()
+  if (cache) {
+    run ();
   } else {
-    login( function( err, data ) {
-      if( err ) { cb( err ) }
-      run()
-    })
+    login (function (err, data) {
+      if (err) { cb (err); }
+      run ();
+    });
   }
-}
+};
 
 // Module
-module.exports = function( setup ) {
-  user.username = setup.username
-  user.password = setup.password
-  return app
-}
+module.exports = function (setup) {
+  user.username = setup.username;
+  user.password = setup.password;
+  return app;
+};
 
 // Communicate
-function talk( props ) {
-  var complete = false
-  function callback( err, res, head ) {
-    if( !complete ) {
-      complete = true
-      props.complete( err, res, head )
+function talk (props) {
+  var complete = false;
+  function callback (err, res, head) {
+    if (!complete) {
+      complete = true;
+      props.complete (err, res, head);
     }
   }
 
@@ -128,80 +123,79 @@ function talk( props ) {
     path: props.path,
     method: props.method || 'GET',
     headers: props.headers || {}
+  };
+
+  options.headers.Referer = 'https://toonopafstand.eneco.nl/index.html';
+  var body = null;
+
+  if (props.query) {
+    options.path += '?'+ querystring.stringify (props.query);
   }
 
-  options.headers.Referer = 'https://toonopafstand.eneco.nl/index.html'
-
-  var body = null
-
-  if( props.query ) {
-    options.path += '?'+ querystring.stringify( props.query )
+  if (props.method === 'POST') {
+    body = querystring.stringify (props.fields);
+    options.headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
+    options.headers['Content-Length'] = body.length;
   }
 
-  if( props.method === 'POST' ) {
-    body = querystring.stringify( props.fields )
-    options.headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8'
-    options.headers['Content-Length'] = body.length
-  }
+  var request = https.request (options);
 
-  var request = https.request( options )
+  request.on ('response', function (response) {
+    var data = [];
+    var size = 0;
 
-  request.on( 'response', function( response ) {
-    var data = []
-    var size = 0
+    response.on ('close', function () {
+      callback (new Error ('request dropped'));
+    });
 
-    response.on( 'close', function() {
-      callback( new Error('request dropped') )
-    })
+    response.on ('data', function (ch) {
+      data.push (ch);
+      size += ch.length;
+    });
 
-    response.on( 'data', function( ch ) {
-      data.push( ch )
-      size += ch.length
-    })
-
-    response.on( 'end', function() {
-      var error = null
-      data = new Buffer.concat( data, size ).toString().trim()
+    response.on ('end', function () {
+      var error = null;
+      data = new Buffer.concat (data, size).toString ().trim ();
       try {
-        data = JSON.parse( data )
-        if( (data.success && data.success === true) || response.statusCode === 200 ) {
-          callback( null, data )
-          return
+        data = JSON.parse (data);
+        if ((data.success && data.success === true) || response.statusCode === 200) {
+          callback (null, data);
+          return;
         } else {
-          error = new Error('api error')
+          error = new Error ('api error');
         }
-      } catch(e) {
-        data = data.replace( /.*-->\s+\{/, '{' );
+      } catch (e) {
+        data = data.replace (/.*-->\s+\{/, '{');
         try {
-          data = JSON.parse( data );
-          error = new Error('api error')
+          data = JSON.parse (data);
+          error = new Error ('api error');
           error.errorCode = data.errorCode;
           error.reason = data.reason;
         }
-        catch(e) {
-          error = new Error('invalid response')
-          error.error = e
+        catch (e) {
+          error = new Error ('invalid response');
+          error.error = e;
         }
       }
 
-      error.data = data
-      error.code = response.statusCode
-      callback( error, null )
-    })
-  })
+      error.data = data;
+      error.code = response.statusCode;
+      callback (error, null);
+    });
+  });
 
-  request.on( 'error', function(e) {
-    var err = new Error('request failed')
-    err.error = e
-    callback( err )
-  })
+  request.on ('error', function (e) {
+    var err = new Error ('request failed');
+    err.error = e;
+    callback (err);
+  });
 
-  request.end( body )
+  request.end (body);
 }
 
 // Get login session
-function login( cb ) {
-  talk({
+function login (cb) {
+  talk ({
     method: 'POST',
     path: '/toonMobileBackendWeb/client/login',
     fields: {
@@ -211,17 +205,17 @@ function login( cb ) {
     headers: {
       Referer: 'https://toonopafstand.eneco.nl/index.html'
     },
-    complete: function( err, data ) {
-      if( err ) { return cb( err ) }
-      cache = data
-      start( cb )
+    complete: function (err, data) {
+      if (err) { return cb (err); }
+      cache = data;
+      start (cb);
     }
-  })
+  });
 }
 
 // Start login session
-function start( cb ) {
-  talk({
+function start (cb) {
+  talk ({
     method: 'GET',
     path: '/toonMobileBackendWeb/client/auth/start',
     query: {
@@ -229,17 +223,17 @@ function start( cb ) {
       clientIdChecksum: cache.clientIdChecksum,
       agreementId: cache.agreements[0].agreementId,
       agreementIdChecksum: cache.agreements[0].agreementIdChecksum,
-      random: guidGenerator(),
-      _: Date.now()
+      random: guidGenerator (),
+      _: Date.now ()
     },
     complete: cb
-  })
+  });
 }
 
 // Build &random= string
-function guidGenerator() {
-  var S4 = function() {
-    return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+function guidGenerator () {
+  var S4 = function () {
+    return (((1+Math.random ())*0x10000)|0).toString (16).substring (1);
   };
   return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
 }
