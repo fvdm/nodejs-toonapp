8985e0b0c11988dae04fad954799394e0ff1a4af
Franklin van de Meent <fr@nkl.in>
2015-02-03 13:43:06 +0100

    Replaced bitwise | with parseInt()

--- app.js
+++ app.js
@@ -238,7 +238,7 @@ function start (cb) {
 // Build &random= string
 function guidGenerator () {
   var S4 = function () {
-    return (((1+Math.random ())*0x10000)|0).toString (16).substring (1);
+    return parseInt (((1+Math.random ())*0x10000)|0).toString (16).substring (1);
   };
   return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
 }
